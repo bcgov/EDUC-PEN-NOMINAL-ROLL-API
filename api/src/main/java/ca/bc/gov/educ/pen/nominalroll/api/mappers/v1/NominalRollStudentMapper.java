94e9fadf3d3acd5db919848ccffcdb0d9e08bf16
OMPRAKASH MISHRA <omprakashmishra3978@gmail.com>
2021-10-14 11:30:26 -0700

    fix mapstruct issue

--- api/src/main/java/ca/bc/gov/educ/pen/nominalroll/api/mappers/v1/NominalRollStudentMapper.java
+++ api/src/main/java/ca/bc/gov/educ/pen/nominalroll/api/mappers/v1/NominalRollStudentMapper.java
@@ -16,7 +16,6 @@ public interface NominalRollStudentMapper {
   @Mapping(target = "validationErrors", ignore = true)
   NominalRollStudent toStruct(ca.bc.gov.educ.pen.nominalroll.api.model.v1.NominalRollStudent nominalRollStudent);
 
-  @Mapping(target = "yearOfProcessing", ignore = true)
   @Mapping(target = "status", constant = "LOADED")
   @Mapping(target = "nominalRollStudentValidationErrors", ignore = true)
   ca.bc.gov.educ.pen.nominalroll.api.model.v1.NominalRollStudent toModel(NominalRollStudent nominalRollStudent);
