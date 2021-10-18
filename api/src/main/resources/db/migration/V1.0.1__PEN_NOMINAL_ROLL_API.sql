eae441bb39b352307bf2d62aefb7e00ddb1b3ca8
arcshiftsolutions <arcshiftsolutions@gmail.com>
2021-10-18 12:57:28 -0700

    Added new student processing table.

---
+++ api/src/main/resources/db/migration/V1.0.1__PEN_NOMINAL_ROLL_API.sql
@@ -0,0 +1,27 @@
+CREATE TABLE NOMINAL_ROLL_POSTED_STUDENT
+(
+    NOMINAL_ROLL_POSTED_STUDENT_ID RAW(16)              NOT NULL,
+    FEDERAL_SCHOOL_BOARD           NUMBER(3)            NOT NULL,
+    FEDERAL_SCHOOL_NUMBER          NUMBER(5)            NOT NULL,
+    FEDERAL_SCHOOL_NAME            VARCHAR2(255)        NOT NULL,
+    AGREEMENT_TYPE                 VARCHAR2(1)          NOT NULL,
+    FEDERAL_BAND_CODE              NUMBER(5)            NOT NULL,
+    FEDERAL_RECIPIENT_BAND_NAME    VARCHAR2(255)        NOT NULL,
+    SURNAME                        VARCHAR2(255)        NOT NULL,
+    GIVEN_NAMES                    VARCHAR2(255),
+    GENDER                         VARCHAR2(1)          NOT NULL,
+    BIRTH_DATE                     DATE                 NOT NULL,
+    GRADE                          VARCHAR2(2)          NOT NULL,
+    FTE                            NUMBER(2,1)          NOT NULL,
+    BAND_OF_RESIDENCE              NUMBER(10)           NOT NULL,
+    ASSIGNED_PEN                   VARCHAR2(9),
+    STATUS                         VARCHAR2(20)         NOT NULL,
+    RECORD_NUMBER                  NUMBER(5)            NOT NULL,
+    PROCESSING_YEAR                DATE                 NOT NULL,
+    CREATE_USER                    VARCHAR2(32)         NOT NULL,
+    CREATE_DATE                    DATE DEFAULT SYSDATE NOT NULL,
+    UPDATE_USER                    VARCHAR2(32)         NOT NULL,
+    UPDATE_DATE                    DATE DEFAULT SYSDATE NOT NULL,
+
+    CONSTRAINT NOMINAL_ROLL_STUDENT_PK PRIMARY KEY (NOMINAL_ROLL_POSTED_STUDENT_ID)
+) TABLESPACE API_PEN_DATA;
