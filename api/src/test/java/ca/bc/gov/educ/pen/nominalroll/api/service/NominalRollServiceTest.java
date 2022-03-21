ab641eb64481a74387a3803c6aafcd046ae36b8c
harshit-kohli <harshit.kohli@gov.bc.ca>
2022-03-21 11:05:53 -0700

    Deleting Closed Fed Prov schools

--- api/src/test/java/ca/bc/gov/educ/pen/nominalroll/api/service/NominalRollServiceTest.java
+++ api/src/test/java/ca/bc/gov/educ/pen/nominalroll/api/service/NominalRollServiceTest.java
@@ -39,7 +39,7 @@ public class NominalRollServiceTest {
   @Autowired
   NominalRollStudentValidationErrorRepository nominalRollStudentValidationErrorRepository;
 
-  @Autowired
+  @Mock
   RestUtils restUtils;
 
   NominalRollService service;
