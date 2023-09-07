bf0957219cfa9fd0ef1a5c08aceff45a49b51d31
SoLetsDev <74216496+SoLetsDev@users.noreply.github.com>
2023-09-07 14:10:38 -0700

    removing debug

--- tools/config/update-configmap.sh
+++ tools/config/update-configmap.sh
@@ -1,4 +1,3 @@
-echo DEBUG:: update config map $1 $2 $3
 envValue=$1
 APP_NAME=$2
 PEN_NAMESPACE=$3
@@ -8,8 +7,6 @@ DB_PWD=$6
 DB_USER=$7
 SPLUNK_TOKEN=$8
 
-echo DEBUG:: PEN_NAMESPACE is $PEN_NAMESPACE
-
 TZVALUE="America/Vancouver"
 SOAM_KC_REALM_ID="master"
 SOAM_KC=soam-$envValue.apps.silver.devops.gov.bc.ca
@@ -17,7 +14,6 @@ SOAM_KC=soam-$envValue.apps.silver.devops.gov.bc.ca
 SOAM_KC_LOAD_USER_ADMIN=$(oc -n $COMMON_NAMESPACE-$envValue -o json get secret sso-admin-${envValue} | sed -n 's/.*"username": "\(.*\)"/\1/p' | base64 --decode)
 SOAM_KC_LOAD_USER_PASS=$(oc -n $COMMON_NAMESPACE-$envValue -o json get secret sso-admin-${envValue} | sed -n 's/.*"password": "\(.*\)",/\1/p' | base64 --decode)
 
-echo DEBUG:: at NATS
 NATS_CLUSTER=educ_nats_cluster
 NATS_URL="nats://nats.${COMMON_NAMESPACE}-${envValue}.svc.cluster.local:4222"
 
